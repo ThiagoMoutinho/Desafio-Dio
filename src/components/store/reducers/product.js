8e6154391ebec7c72fe75d22fb4b627181c6c50d
Thiago Moutinho <thiagomoutinho34@gmail.com>
2022-02-02 18:36:13 -0300

    Desafio Dio

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Paysandu 2022",
+      price: 400,
+      image: "./images/produtos/paysandu.jpg",
+      id_categorys: 1,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Remo 2022",
+      price: 150,
+      image: "./images/produtos/remo.jpg",
+      id_categorys: 1,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "tuna 2022",
+      price: 230,
+      image: "./images/produtos/tuna.jpg",
+      id_categorys: 1,
+      name_categorys: "Clubes brasileiros",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "nacional",
+      fk_idcategorys: 2,
+      name_product: "Coritiba 2021/22",
+      price: 299.99,
+      image: "./images/produtos/coritiba.jpg",
+      id_categorys: 2,
+      name_categorys: "Clube Paranaense",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Manchester City 2020/21",
+      price: 350.99,
+      image: "./images/produtos/man-city.jpg",
+      id_categorys: 2,
+      name_categorys: "Clubes europeus",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Milan 2020/21",
+      price: 299.99,
+      image: "./images/produtos/milan.jpg",
+      id_categorys: 2,
+      name_categorys: "Clubes europeus",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Brasil 1994",
+      price: 200,
+      image: "./images/produtos/brasil94.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Flamengo 1981",
+      price: 450,
+      image: "./images/produtos/fla81.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "selecao",
+      fk_idcategorys: 4,
+      name_product: "Alemanha 2020",
+      price: 350,
+      image: "./images/produtos/alemanha.jpg",
+      id_categorys: 4,
+      name_categorys: "Seleções nacionais",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "selecao",
+      fk_idcategorys: 4,
+      name_product: "Brasil 2020",
+      price: 399,
+      image: "./images/produtos/brasil.jpg",
+      id_categorys: 4,
+      name_categorys: "Seleções nacionais",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "casual",
+      fk_idcategorys: 5,
+      name_product: "Flamengo T 2020",
+      price: 180,
+      image: "./images/produtos/fla_treino.jpg",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "casual",
+      fk_idcategorys: 5,
+      name_product: "Real Madrid 2019/20",
+      price: 189.99,
+      image: "./images/produtos/real_treino.jpg",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+];
+
+export default productsList;
